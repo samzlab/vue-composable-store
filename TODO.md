88ea8e8f7bdcd03eb4816aeb2f7775bbe05a663d
Kövesdi György <hello@samzlab.hu>
2020-10-05 13:31:38 +0200

    build: use rollup once again

--- TODO.md
+++ TODO.md
@@ -2,6 +2,8 @@
 
 * [ ] wrap asserts in `if(__DEV__)` blocks
 * [ ] use rollup
+* [ ] eslint-typescript check
+* [ ] test negative branches
 * [ ] accept options in the plugin initialization: `app.use(store, { plugins?: [] })`
 * [ ] plugin hooks
 * [ ] example projects
---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import filesize from 'rollup-plugin-filesize';
+
+// @ts-ignore
+import { peerDependencies } from './package.json'
+
+const external = [ 'vue', ...Object.keys(peerDependencies) ];
+
+/** @type {import('rollup').RollupOptions['output']} */
+const output = {
+    format: 'esm',
+    dir: 'dist',
+    entryFileNames: '[name]-[format].js'
+};
+
+
+/** @type {import('rollup').RollupOptions} */
+const options = {
+    input: 'src/index.ts',
+    external,
+    output,
+    plugins: [
+        nodeResolve(),
+        typescript({
+            exclude: '__tests__/*'
+        }),
+        filesize()
+    ]
+};
+
+export default [
+	{
+        ...options,
+        output: {
+            ...output,
+            format: 'cjs'
+        }
+    },
+    /** @type {import('rollup').RollupOptions} */
+	{
+		...options,
+		output
+	}
+];
